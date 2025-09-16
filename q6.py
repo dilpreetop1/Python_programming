7cccc2aa01e2439b3be730867f07f908ad8ed340
dilpreetop1 <2024d2r013@mietjammu.in>
2025-09-16 15:23:14 +0530

    Add files via upload

---
+++ q6.py
@@ -0,0 +1,14 @@
+n = int(input("enter the number"))
+
+if n==1:
+    print("one")
+elif n==2:
+    print("two")
+elif n==3:
+    print("three")
+elif n==4:
+    print("four")
+elif n==5:
+    print("five")
+else:
+    print("invalid number")
