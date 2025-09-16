16d089b8a5eb741791500d869ec1da0576177d54
dilpreetop1 <2024d2r013@mietjammu.in>
2025-09-16 15:48:16 +0530

    Add files via upload

---
+++ q8.py
@@ -0,0 +1,14 @@
+s = int(input("enter the registration number:"))
+
+if s>0 and s<=10:
+    print("ROW 1")
+elif s>=11 and s<=20:
+    print("ROW 2")
+elif s>=21 and s<=30:
+    print("ROW 3")
+elif s>=31 and s<=40:
+    print("ROW 4")
+elif s>=41 and s<=50: 
+    print("ROW 5")
+elif s>=51 and s<=61:
+    print("ROW 6")
