89b18b902677b31ebe6e0cc66280731ba7f7c47a
dilpreetop1 <2024d2r013@mietjammu.in>
2025-09-17 10:21:52 +0530

    Add files via upload

---
+++ q13.py
@@ -0,0 +1,8 @@
+n = input("enter the character:")
+
+if n==n.lower():
+    print("characters are in small letters")
+elif n==n.upper():
+    print("characters are in upper letters")
+else:
+    print("invalid")
