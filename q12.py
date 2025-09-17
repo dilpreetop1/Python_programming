d69c8212e1fe3cf2d2b61f4301cdf536e93d4fed
dilpreetop1 <2024d2r013@mietjammu.in>
2025-09-17 10:14:41 +0530

    Add files via upload

---
+++ q12.py
@@ -0,0 +1,10 @@
+n = int(input("enter the number:"))
+match n:
+    case 1:
+        print("HI")
+    case 2:
+        print("WELCOME")
+    case 3:
+        print("BYE")
+    case _:
+        print("INVALID NUMBER")
