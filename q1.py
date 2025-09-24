dd968b7a83538e2484d62340a78dcddad26ef69e
dilpreetop1 <2024d2r013@mietjammu.in>
2025-09-24 10:58:12 +0530

    Add files via upload

--- q1.py
+++ q1.py
@@ -1,2 +1,19 @@
-name= input("enter the user name :")
-print("WELCOME :", name)
+
+# Ask the user to enter the basic salary
+basic_salary = float(input("Enter basic salary: "))
+
+# Calculate allowances and deductions
+hra = 0.20 * basic_salary  # HRA is 20% of basic
+da = 0.10 * basic_salary   # DA is 10% of basic
+pf = 0.12 * basic_salary   # PF is 12% of basic
+
+# Calculate gross and net salary
+gross_salary = basic_salary + hra + da
+net_salary = gross_salary - pf
+
+# Show the results
+print("HRA =", hra)
+print("DA =", da)
+print("PF =", pf)
+print("Gross Salary =", gross_salary)
+print("Net Salary =", net_salary)
--- q2.py
+++ q2.py
@@ -1,8 +1,11 @@
-n1= int(input("enter the first number"))
-n2= int(input("enter the second number"))
-n3= int(input("enter the third number"))
+age=int(input("enter the age:"))
+m_income= int(input("enter the monthly income:"))
+existing_loan= float(int(input("enter the loan amount:")))
+c_score =  float(input("enter the cibil score:"))
 
-sum= n1+n2+n3
-print(sum)
-average= sum/3
-print(average)
+
+#loan_amount = income * 0.5
+if age>=21 and age<=60 and m_income>25000  and c_score>700 and existing_loan<=m_income/2:
+    print("eligible for loan")
+else:
+    print("not eligible for loan")
